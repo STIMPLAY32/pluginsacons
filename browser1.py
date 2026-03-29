4fee33a46aa5fecea69efca1dd563db37832ba5f
STIMPLAY32 <zakir.ahmadov2009@gmail.com>
2026-03-29 19:00:10 +0300

    Delete browser1.py

--- browser1.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-from urllib.parse import quote
-
-def browser1():
-    search_query = input('openbrowser > Enter text for search >> ')
-    encoded_query = quote(search_query)  # кодируем кириллицу в URL
-    url = f"https://www.google.com/search?q={encoded_query}"
-    webbrowser.open(url)
