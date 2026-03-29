2a7c8ea7682b946a839d7bd9ac2f16d9cd2360f1
STIMPLAY32 <zakir.ahmadov2009@gmail.com>
2026-03-29 18:28:49 +0300

    Modify browser function to handle Cyrillic input
    Updated search function to encode Cyrillic characters in URL.

--- browser.py
+++ browser.py
@@ -1,5 +1,8 @@
 import webbrowser
+from urllib.parse import quote
+
 def browser():
-  search_query = input('Enter text for search >>')
-  url = f"https://www.google.com/search?q={search_query}"
-  webbrowser.open(url)
+    search_query = input('openbrowser > Enter text for search >> ')
+    encoded_query = quote(search_query)  # кодируем кириллицу в URL
+    url = f"https://www.google.com/search?q={encoded_query}"
+    webbrowser.open(url)
