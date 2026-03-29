bf3c8fd956c8363b53623126771f8c5479e5905e
STIMPLAY32 <zakir.ahmadov2009@gmail.com>
2026-03-29 18:21:50 +0300

    Update browser.py

--- browser.py
+++ browser.py
@@ -1,5 +1,5 @@
 import webbrowser
 def browser():
-  search_query = input(name + '@root > openbrowser > Enter text for search >>')
+  search_query = input('Enter text for search >>')
   url = f"https://www.google.com/search?q={search_query}"
   webbrowser.open(url)
