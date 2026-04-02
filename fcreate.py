aab6fc5d2cdd1bc7cf62b3af72fb0eb2f4e5554d
STIMPLAY32 <zakir.ahmadov2009@gmail.com>
2026-04-02 14:44:39 +0300

    Update fcreate.py

--- fcreate.py
+++ fcreate.py
@@ -3,7 +3,7 @@ def fcreate():
   folder_name = input('Enter folder name >> ')
   file_name = input('Enter file name >> ')
   file_path = os.path.join(folder_name, file_name)
-  text = input('Enter text')
+  text = input('Enter text >> ')
   os.makedirs(folder_name, exist_ok=True)
   with open(file_path, 'w', encoding='utf-8') as f:
       f.write(text)
